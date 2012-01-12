f3d1819fd8a7c4b0b4fd1854c6ef9d83913b5c15
John Dyer <lbrapid@gmail.com>
2012-01-12 15:52:20 -0500

    Use number_to_currency now that product_price is deprecated

--- app/overrides/replace_products.rb
+++ app/overrides/replace_products.rb
@@ -4,21 +4,18 @@ Deface::Override.new(:virtual_path => %q{spree/shared/_products},
                           :closing_selector => %q{},
                           :text => %q{<% @body_id = 'product-list' %>
 
-<%= render 'spree/shared/product_banner' %>
-
 <div class="list" id="products" data-hook>
 <% products.each do |product| %>
   <% if Spree::Config[:show_zero_stock_products] || product.has_stock? %>
     <div class="product">
       <h2><%= link_to product.name, product_path(product) %></h2>
       <p class="image">
-        <%= link_to product_image(product), product %>
       </p>
       <div class="details">
         <p class="stock"><%= product.has_stock? ? "IN STOCK" : "OUT OF STOCK" %></p>
         <p class="prices">
           <span class="price discounted"><%= number_to_currency(product.price * 1.2) %></span>&nbsp;
-          <span class="price selling"><%= product_price(product) %></span></p>
+          <span class="price selling"><%= number_to_currency(product.price) %></span></p>
         <p><%= link_to 'View More', product_path(product), :class => 'button' %></p>
       </div>
       <p class="description">
