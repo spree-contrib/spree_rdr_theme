af9db26822dfeafb29d25d812565f399bc24eaf1
Alexey Topolyanskiy <devilcoders@gmail.com>
2012-02-10 12:12:03 +0200

    More updates for 1.0.x

--- app/overrides/add_taxonomies_to_product_show.rb
+++ app/overrides/add_taxonomies_to_product_show.rb
@@ -3,5 +3,5 @@ Deface::Override.new(:virtual_path => %q{spree/products/show},
                      :insert_after => %q{[data-hook='product_show']},
                      :sequence => {:after => 'add_recently_viewed_products_to_products_show'},
                      :text => %q{<%= content_for :sidebar do %>
-  <%= render 'shared/all_products' %>
+  <%= render 'spree/shared/all_products' %>
 <% end %>})
