3b1d2feca62d0bfc84e4b3b3e1dc284bff688405
Will Emerson <wemerson@livelihoodtech.com>
2012-02-28 17:33:41 -0800

    added stylesheets to install_generator

--- lib/generators/spree_rdr_theme/install/install_generator.rb
+++ lib/generators/spree_rdr_theme/install/install_generator.rb
@@ -7,6 +7,10 @@ module SpreeRdrTheme
         append_file "app/assets/javascripts/store/all.js", "//= require store/rdr_wishlist\n" 
       end
       
+      def add_stylesheets
+        inject_into_file "app/assets/stylesheets/store/all.css", " *= require store/rdr\n", :before => /\*\//, :verbose => true
+      end
+      
     end
   end
 end
