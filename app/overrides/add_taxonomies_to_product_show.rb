6dac574fce28cd05744bc00a215b184e2ffb5fb8
Brian D. Quinn <briandquinn@gmail.com>
2012-05-27 20:48:04 +0100

    Work in progress converting to use Deface DSL

--- app/overrides/add_taxonomies_to_product_show.rb
+++
@@ -1,7 +0,0 @@
-Deface::Override.new(:virtual_path => %q{spree/products/show},
-                     :name => %q{add_taxonomies_to_product_show},
-                     :insert_after => %q{[data-hook='product_show']},
-                     :sequence => {:after => 'add_recently_viewed_products_to_products_show'},
-                     :text => %q{<%= content_for :sidebar do %>
-  <%= render 'spree/shared/all_products' %>
-<% end %>})
--- app/overrides/remvove_address_buttons.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(:virtual_path => %q{spree/checkout/_address},
-                          :name => %q{remove_address_buttons},
-                          :remove => %q{[data-hook='buttons']},
-                          :closing_selector => %q{})
