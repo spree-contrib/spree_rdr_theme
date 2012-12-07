70e4dde599ed0b7c7a92cb98c3f9bc7058b3cff3
Weston Platter <westonplatter@gmail.com>
2012-12-07 00:51:53 -0600

    stop repeating products on taxon pages

--- app/overrides/spree/taxons/replace_taxon_children.rb
+++ app/overrides/spree/taxons/replace_taxon_children.rb
@@ -1,6 +1,3 @@
 Deface::Override.new(:virtual_path => %q{spree/taxons/show},
                      :name => %q{replace_taxon_children},
-                     :replace => %q{[data-hook='taxon_children']},
-                     :text => %q{<div data-hook="taxon_products">
-    <%= render :partial => "spree/shared/products", :locals => {:products => @products, :taxon => @taxon } %>
-  </div>})
+                     :remove => %q{[data-hook='taxon_children']})
